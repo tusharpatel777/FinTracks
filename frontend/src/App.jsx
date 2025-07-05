68815509db8de25e88fa5643966ca58c0ace3c02
tusharpatel777 <tussipatel826@gmail.com>
2025-07-05 12:22:34 +0530

    updation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,10 @@
-// import { Routes, Route } from 'react-router-dom';
-// import { AuthProvider } from './context/AuthContext';
-// import { TransactionProvider } from './context/TransactionContext';
 
-// import Header from './components/Header';
-// import PrivateRoute from './components/PrivateRoute';
-// import Dashboard from './pages/Dashboard';
-// import Login from './pages/Login';
-// import Register from './pages/Register';
-
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <TransactionProvider>
-//         <div className="bg-slate-100 min-h-screen">
-//           <Header />
-//           <main className="container mx-auto px-4 py-8">
-//             <Routes>
-//               <Route path="/login" element={<Login />} />
-//               <Route path="/register" element={<Register />} />
-//               <Route path="/" element={
-//                   <PrivateRoute>
-//                     <Dashboard />
-//                   </PrivateRoute>
-//                 }
-//               />
-//             </Routes>
-//           </main>
-//         </div>
-//       </TransactionProvider>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext.jsx'; // <-- Import ThemeProvider
 import { AuthProvider } from './context/AuthContext.jsx';
 import { TransactionProvider } from './context/TransactionContext.jsx';
 
-import Header from './components/Header.jsx';
+import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
